b1fac414ca8ba06d5b7d00bec4f28168bde25603
leider <derleider@web.de>
2020-02-28 09:11:18 +0100

    replace spinner by "native" ones

--- src/widgets/McbCount.vue
+++ src/widgets/McbCount.vue
@@ -17,23 +17,6 @@ export default class McbCount extends Vue {
   @Prop() value!: number;
   @Prop() readonly name!: string;
   @Prop() readonly label!: string;
-
-  get valueString() {
-    return (this.value || 0).toString();
-  }
-  set valueString(val) {
-    this.$emit("input", parseInt(val, 10));
-  }
-
-  decrease() {
-    if (this.value > 0) {
-      this.$emit("input", this.value - 1);
-    }
-  }
-
-  increase() {
-    this.$emit("input", this.value + 1);
-  }
 }
 </script>
 
